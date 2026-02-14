7e12fc4dcf78015dd2999e544347820303f5d725
iisacc-Justmoong <andudyun0504@gmail.com>
2026-02-14 15:54:56 +0900

    Force Vulkan graphics backend across examples and main application
    - Implement Vulkan backend enforcement with loader checks in main files.
    - Update `CMakeLists.txt` to replace dummy AGL framework with an Apple GL link shim.
    - Fail gracefully if Vulkan support is unavailable.

--- example/EventListener/main.cpp
+++ example/EventListener/main.cpp
@@ -1,13 +1,51 @@
 #include <QCoreApplication>
 #include <QGuiApplication>
+#include <QLibrary>
+#include <QQuickWindow>
 #include <QQuickStyle>
 #include <QQmlApplicationEngine>
+#include <QSGRendererInterface>
 #include <QtPlugin>
 
 Q_IMPORT_PLUGIN(LVRSPlugin)
 
+namespace {
+bool enforceVulkanBackend()
+{
+    qputenv("QSG_RHI_BACKEND", QByteArrayLiteral("vulkan"));
+    qputenv("QSG_RHI_PREFER_SOFTWARE_RENDERER", QByteArrayLiteral("0"));
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::Vulkan);
+
+    const char *kVulkanLoaders[] = {
+#if defined(Q_OS_WIN)
+        "vulkan-1"
+#elif defined(Q_OS_MACOS)
+        "vulkan",
+        "MoltenVK",
+        "libvulkan.1",
+        "libMoltenVK"
+#else
+        "vulkan",
+        "libvulkan.so.1"
+#endif
+    };
+
+    for (const char *loaderName : kVulkanLoaders) {
+        QLibrary vulkanLoader(QString::fromLatin1(loaderName));
+        if (vulkanLoader.load()) {
+            vulkanLoader.unload();
+            return true;
+        }
+    }
+    return false;
+}
+}
+
 int main(int argc, char *argv[])
 {
+    if (!enforceVulkanBackend())
+        return -1;
+
     QQuickStyle::setStyle(QStringLiteral("Basic"));
     QGuiApplication app(argc, argv);
 
--- main.cpp
+++ main.cpp
@@ -34,26 +34,47 @@ void loadBundledFonts()
     }
 }
 
-void configureGraphicsBackend()
+bool enforceVulkanBackend()
 {
-    QLibrary vulkanLoader(QStringLiteral("vulkan"));
-    if (vulkanLoader.load()) {
-        vulkanLoader.unload();
-        QQuickWindow::setGraphicsApi(QSGRendererInterface::Vulkan);
-        qInfo() << "LVRS graphics backend preference: Vulkan";
-        return;
+    qputenv("QSG_RHI_BACKEND", QByteArrayLiteral("vulkan"));
+    qputenv("QSG_RHI_PREFER_SOFTWARE_RENDERER", QByteArrayLiteral("0"));
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::Vulkan);
+
+    const char *kVulkanLoaders[] = {
+#if defined(Q_OS_WIN)
+        "vulkan-1"
+#elif defined(Q_OS_MACOS)
+        "vulkan",
+        "MoltenVK",
+        "libvulkan.1",
+        "libMoltenVK"
+#else
+        "vulkan",
+        "libvulkan.so.1"
+#endif
+    };
+
+    for (const char *loaderName : kVulkanLoaders) {
+        QLibrary vulkanLoader(QString::fromLatin1(loaderName));
+        if (vulkanLoader.load()) {
+            vulkanLoader.unload();
+            qInfo() << "LVRS graphics backend: Vulkan (forced), loader =" << loaderName;
+            return true;
+        }
     }
 
-    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
-    qWarning() << "LVRS graphics backend fallback: OpenGL";
+    qCritical() << "No Vulkan loader found. LVRS requires Vulkan rendering.";
+    return false;
 }
 }
 
 int main(int argc, char *argv[])
 {
     RenderQuality::configureGlobalDefaults();
+    if (!enforceVulkanBackend())
+        return -1;
+
     QGuiApplication app(argc, argv);
-    configureGraphicsBackend();
     app.setApplicationName(QStringLiteral("LVRS"));
     loadBundledFonts();
     FontPolicy::installPretendardFallbacks();
